54986fc155b80f29a40b477b23192ed9ff0fa2e3
Ger Hobbelt <ger@hobbelt.com>
2013-02-21 08:08:55 +0100

    whitespace police: trim all WS; UNIX LF; tabs to spaces.

--- tests/all-tests.js
+++ tests/all-tests.js
@@ -1,4 +1,4 @@
 exports.testRegExpLexer = require("./regexplexer");
 
 if (require.main === module)
-    process.exit(require("test").run(exports)); 
+    process.exit(require("test").run(exports));
